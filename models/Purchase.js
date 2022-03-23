59c73b64b6bb36f74076b621f3d8d7a7e29d13ef
Ruchini <ruchiniirushika@yahoo.com>
2022-03-23 14:07:51 +0530

    newly create purchase table

---
+++ models/Purchase.js
@@ -0,0 +1,47 @@
+
+const mongoose = require("mongoose");
+const PurchaseSchema = new mongoose.Schema({
+    
+
+    invoice_date:{
+        type:Date,
+        required:true
+    },
+    item_name:{
+        type:String,
+        required:true,
+    },
+
+    unit_price:{
+        type:Float32Array,
+        required:true,
+    },
+
+    quantity:{
+        type:Number,
+        required:true,
+    },
+
+    total_amount:{
+        type:Float32Array,
+        required:true,
+    },
+
+    supplier:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Supplier",
+        required:true,
+    },
+
+    item:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Item",
+        required:true,
+    },
+
+    
+      
+    
+})
+
+module.exports=mongoose.model("Purchase",PurchaseSchema)
