febe5b6e7e564c42de8c8d20bf5ca146ae3ee74a
Kavindu82 <it20637682@my.sliit.lk>
2022-03-24 17:33:00 +0530

    update item

--- routes/item-manage.js
+++ routes/item-manage.js
@@ -13,8 +13,22 @@ router.get("/get-items" , async(req,res)=>{
 })
 
 router.post("/add-item" , async(req,res)=>{
-console.log(req.body)    
+    var itemname=req.body.itmname
+    var ItemCategory=req.body.itmcat
+    var quantity=req.body.itmquant
+    var reorder_level=req.body.itmreorder
+    var  sup_id=req.body.itmsup
+    var  invoice_no=req.body.itminvoice
+
+    var newItem= new ItemSchema({item_name:itemname,category_name:ItemCategory,quantity:quantity,reorder_level:reorder_level,sup_id:sup_id ,invoice_no:invoice_no})
+    newItem.save(function(err,result){
+        if(err){
+            res.json({msg:err})
+        }else{
+            res.json({mag:"item  created"})
+        }   
 })
 
+})
 
 module.exports = router;
