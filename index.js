7f6813d266c8a3969d73bd4f5f871c703622de87
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-03-24 15:13:28 +0530

    index changed

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const { DB, PORT } = require('./config')
 const app = express()
 
 app.use(cors())
-app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
 
 app.use("/api/employee-manage", require("./routes/employee-manage"))
 app.use("/api/project-manage", require("./routes/project-manage"))
