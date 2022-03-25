96bb23fd4c89608277fee731cf34b1e54f0fda8b
chamara <lasithachamara0@gmail.com>
2022-03-25 12:13:27 +0530

    employee,bankaccount,department

--- routes/bankaccount-manage.js
+++ routes/bankaccount-manage.js
@@ -12,5 +12,30 @@ router.get("/get-BankAccount" , async(req,res)=>{
     })
 })
 
+router.post("/add-BankAccount" , async(req,res)=>{
+    //console.log(req.body)  
+    var bank_account_no = req.body.bank_account_no;
+    var bank_name = req.body.bank_name;
+    var branch_name = req.body.branch_name;
+    var account_holder_name = req.body.account_holder_name;
+
+    var newBankAccount = new BankAccountSchema({
+        bank_account_no:bank_account_no,
+        bank_name:bank_name,
+        branch_name:branch_name,
+        account_holder_name:account_holder_name,
+      
+    })
+    
+    newBankAccount.save(function(err,result){
+        if(err){
+            res.json({msg:err})
+        }else{
+            res.json({msg:"Completed"})
+        }
+    })
+    
+   })
+
 
 module.exports = router;
--- routes/department-manage.js
+++ routes/department-manage.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const DepartmentSchema = require("../models/Department")
 
-router.get("/get-department" , async(req,res)=>{
+router.get("/get-Department" , async(req,res)=>{
     let Department = DepartmentSchema.find({} , function(err , Department){
         if(err){
             res.json({msg:err})
@@ -11,6 +11,31 @@ router.get("/get-department" , async(req,res)=>{
         }
     })
 })
+
+router.post("/add-Department" , async(req,res)=>{
+   // console.log(req.body)  
+   var job_dept = req.body.job_dept;
+   var Dep_name = req.body.Dep_name;
+   var description = req.body.description;
+   var salary_rang = req.body.salary_rang; 
+    
+
+   var newDepartment = new DepartmentSchema({
+    job_dept:job_dept,
+    Dep_name:Dep_name,
+    description:description,
+    salary_rang:salary_rang,
+  
+})
+newDepartment.save(function(err,result){
+    if(err){
+        res.json({msg:err})
+    }else{
+        res.json({msg:"Completed"})
+    }
+})
+})
+
 
 
 module.exports = router;
