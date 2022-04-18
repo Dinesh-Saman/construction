af105795bdf3d5e4a2b4e61e6063e513e0186754
chamara <lasithachamara0@gmail.com>
2022-04-18 23:00:17 +0530

    commit

--- routes/Client-manage.js
+++ routes/Client-manage.js
@@ -14,33 +14,35 @@ router.get("/get-Client", async(req, res) => {
 
 router.post("/add-client" , async(req,res)=>{
     // console.log(req.body)
-    let fName = req.body.first_name;
-    let lName = req.body.last_name;
+    let first_name = req.body.first_name;
+    let last_name = req.body.last_name;
     let address = req.body.address;
     let contact_no = req.body.contact_no;
     let email = req.body.email;
-    let nic = req.body.nic
+    let nic_no = req.body.nic_no;
+    let proj_id = req.body.proj_id;
 
     let newClient = ClientSchema({
-        first_name:fName,
-        last_name:lName,
-        address,
+        first_name:first_name,
+        last_name:last_name,
+        address:address,
         contact_no,
-        email,
-        nic_no:nic
+        email:email,
+        nic_no:nic_no,
+        proj_id:proj_id
     })
 
-    // newClient.save(function(err,result){
-    //     if(err){
-    //         res.json({msg:err})
-    //     }else{
-    //         res.json({msg:"Completed"})
-    //     }
-    // })
+    newClient.save(function(err,result){
+        if(err){
+            res.json({msg:err})
+        }else{
+            res.json({msg:"Completed"})
+        }
+    })
+    
+    })
 
-    console.log(req)
 
-})
 
 
 module.exports = router;
