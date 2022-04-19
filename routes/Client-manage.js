be16d4790578123dcf346e8981b7dd3f0142b9a3
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-04-19 13:01:36 +0530

    pull chamara

--- routes/Client-manage.js
+++ routes/Client-manage.js
@@ -23,39 +23,33 @@ router.post("/add-client", async(req, res) => {
     let proj_id = req.body.proj_id;
 
     let newClient = ClientSchema({
-<<<<<<< HEAD
         first_name: fName,
         last_name: lName,
         address,
         contact_no,
         email,
-        nic_no: nic
-=======
-        first_name:first_name,
-        last_name:last_name,
-        address:address,
+        nic_no: nic,
+        first_name: first_name,
+        last_name: last_name,
+        address: address,
         contact_no,
-        email:email,
-        nic_no:nic_no,
-        proj_id:proj_id
->>>>>>> af105795bdf3d5e4a2b4e61e6063e513e0186754
+        email: email,
+        nic_no: nic_no,
+        proj_id: proj_id
     })
 
-    newClient.save(function(err,result){
-        if(err){
-            res.json({msg:err})
-        }else{
-            res.json({msg:"Completed"})
+    newClient.save(function(err, result) {
+        if (err) {
+            res.json({ msg: err })
+        } else {
+            res.json({ msg: "Completed" })
         }
     })
-<<<<<<< HEAD
 
     // console.log(req)
-=======
-    
-    })
 
->>>>>>> af105795bdf3d5e4a2b4e61e6063e513e0186754
+})
+
 
 
 
