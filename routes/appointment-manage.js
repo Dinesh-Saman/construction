a5ecc1e84597e6d9f6643c76ab6d88d4d30f1e73
Akilatharuka97 <akilatharuka555@gmail.com>
2022-04-19 22:15:17 +0530

    Merge branch 'tharushi' of https://github.com/Dinesh-Saman/construction into Akila

--- routes/appointment-manage.js
+++ routes/appointment-manage.js
@@ -12,5 +12,34 @@ router.get("/get-Appointment" , async(req,res)=>{
     })
 })
 
+router.post("/add-Appointment", async(req, res) => {
+    // console.log(req.body)
+    let first_name = req.body.first_name;
+    let last_name = req.body.last_name;
+    let address = req.body.address;
+    let contact_no = req.body.contact_no;
+    let email = req.body.email;
+    let nic_no = req.body.nic_no;
+    let proj_id = req.body.proj_id;
+
+    let newClient = ClientSchema({
+        first_name:first_name,
+        last_name:last_name,
+        address:address,
+        contact_no,
+        email:email,
+        nic_no:nic_no,
+        proj_id:proj_id
+    })
+
+    newClient.save(function(err,result){
+        if(err){
+            res.json({msg:err})
+        }else{
+            res.json({msg:"Completed"})
+        }
+    })
+    
+    })
 
 module.exports = router;
